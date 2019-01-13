de9324e9781cad86f278332eefa188d44c3dcc8e
mikemikemike451 <46022223+mikemikemike451@users.noreply.github.com>
2019-01-13 21:05:04 +0800

    Revert "20190113"
    This reverts commit 03d5b49ecb1adf0cb40bc7d21a72108a12519c11.

--- 1a2b.py
+++ 1a2b.py
@@ -1,4 +1,5 @@
 import random
+import os
 playornot = True
 
 
@@ -58,7 +59,8 @@ while playornot:
 				break
 		else:
 			print("請再輸入一次！")
-
+def backtomainpage():
+	os.system('C:\\Users\\mikem\\Desktop\\Final-Project-Team-10\\PBC-Final-Project-Team-10\\20190107介面.py')
 while starttoback:
 	backornot = input("是否回到主程式? Y/N")
 	if backornot == 'Y' or backornot == 'y':
